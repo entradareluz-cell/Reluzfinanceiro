e2bc0fc6c10a591f62866c78ad6b32b076ba81dd
entradareluz-cell <entradareluz@gmail.com>
2026-08-18 08:18:22 -0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyBWuAqwQjqZCdxlNSJc_nXaLL598EI4UvU",
-  authDomain: "reluz-financeiro.firebaseapp.com",
-  projectId: "reluz-financeiro",
-  storageBucket: "reluz-financeiro.firebasestorage.app",
-  messagingSenderId: "1085388395323",
-  appId: "1:1085388395323:web:729589c9222c8e77025d1e"
-};
